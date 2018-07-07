d15a24d9a0c8ee5e550cc7a0ea92a3729f441fe5
arskaout <aaron.haarti@gmail.com>
2018-07-08 01:42:16 +0300

    Add files via upload

--- fiksit.js
+++ fiksit.js
@@ -1,14 +1,28 @@
-var coll = document.getElementsByClassName("collapsible");
+var b_b = document.getElementsByClassName("brand_button");
+var b = document.getElementsByClassName("brand");
 var i;
+var z;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+for (i = 0; i < b_b.length; i++) {
+    b_b[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
             content.style.display = "block";
+        }
+    });
+}
+
+for (z = 0; z < b.length; z++) {
+    b[z].addEventListener("click", function() {
+        this.classList.toggle("bactive");
+        var acc = this.nextElementSibling;
+        if (acc.style.display === "block") {
+            acc.style.display = "none";
+        } else {
+            acc.style.display = "block";
         }
     });
 }
